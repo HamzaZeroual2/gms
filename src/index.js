8d130e1733b25c24d39241b8c4e2f3c163beda48
Hamza Zeroual <hamzaxboy11@gmail.com>
2023-07-17 12:29:47 +0100

    add users list fix height

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+
 import Login from './scenes/global/Login';
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
